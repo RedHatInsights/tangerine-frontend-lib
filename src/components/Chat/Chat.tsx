82d01da7d905417538b114b4948b12318d832cc8
Matt Knop <mknop@redhat.com>
2024-08-01 12:25:39 -0600

    first commit

---
+++ src/components/Chat/Chat.tsx
@@ -0,0 +1,137 @@
+import { useState, useEffect } from "react"
+import { useParams } from "react-router-dom"
+import axios from "axios"
+import { TextContent, Text, TextVariants, TextInput } from "@patternfly/react-core"
+import Markdown from "react-markdown"
+import remarkGfm from "remark-gfm"
+import SearchInfo from "../SearchInfo"
+
+export const Chat = () => {
+    const { agentId } = useParams()
+    const [agentInfo, setAgentInfo] = useState({agent_name: '', description: ''})
+    const [messages, setMessages] = useState([])
+    const [chatInput, setChatInput] = useState('')
+
+    const addMessage = (sender, message, done = false) => {
+        const messageToAdd = {sender: sender, text: message, done: done}
+        setMessages(prevMessages => [...prevMessages, messageToAdd])
+    }
+
+    const handleChatKeyDown = (event) => {
+        if (event.key == 'Enter' && chatInput.length > 0) {
+            addMessage('human', chatInput)
+            setChatInput('')
+            sendChatMessage()
+        }
+    }
+
+    useEffect(() => {
+        getAgentInfo();
+      }, []);
+
+    const getAgentInfo = () => {
+        axios.get(`/api/agents/${agentId}`)
+          .then(response => {
+            setAgentInfo(response.data)
+          })
+          .catch(error => {
+            console.error('Error fetching agents:', error);
+          });
+    };
+
+    const sendChatMessage = async () => {
+        // Make a POST request with streaming response
+        const response = await fetch(`/api/agents/${agentId}/chat`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                query: chatInput,
+                prevMsgs: messages,
+                stream: "true",
+            })
+        })
+
+        const reader = response.body.pipeThrough(new TextDecoderStream("utf-8")).getReader();
+
+        while(true) {
+            const chunk = await reader.read();
+            const {done, value} = chunk;
+
+            if (done) {
+                break;
+            }
+
+            const matches = [...value.matchAll(/data: (\{.*\})\r\n/g)];
+
+            for (const match of matches) {
+                const jsonString = match[1]
+                const { text_content, search_metadata } = JSON.parse(jsonString);
+
+                if (text_content || search_metadata) {
+                    setMessages((prevMessages) => {
+                        const lastMessage = prevMessages[prevMessages.length - 1]
+                        if (lastMessage.sender !== "ai") {
+                            const newMessage = {sender: "ai", text: text_content, done: false}
+                            return [...prevMessages, newMessage]
+                        }
+
+                        const updatedMessages = [...prevMessages];
+
+                        if (text_content) {
+                            updatedMessages[updatedMessages.length - 1].text += text_content
+                        }
+
+                        if (search_metadata) {
+                            updatedMessages[updatedMessages.length - 1].search_metadata = search_metadata
+                            // search_metadata arrives from API at the end of the text content stream
+                            updatedMessages[updatedMessages.length - 1].done = true
+                        }
+
+                        return updatedMessages;
+                    })
+                }
+            }
+        }
+    };
+
+
+    return (
+        <>
+            <TextContent style={{"marginLeft": "10rem", "paddingTop": "2rem", "paddingBottom": "2rem"}}>
+                <Text component={TextVariants.h1}>Chat with {agentInfo.agent_name}</Text>
+                <Text component={TextVariants.p}>{agentInfo.description}</Text>
+            </TextContent>
+            <div>
+                <div className="pf-v5-c-panel pf-m-scrollable" style={{"marginLeft": "10rem", "marginRight": "15rem", "display": "flex", "flexDirection": "column", "justifyContent": "space-around"}}>
+                    <div className="pf-v5-c-panel__main" style={{"minHeight": "70vh"}}>
+                        <div className="pf-v5-c-panel__main-body">
+                            {
+                                messages && messages.map((message, index) => (
+                                    <TextContent key={index} style={{"paddingBottom": "1rem"}}>
+                                        <Text component={TextVariants.h3}>{message.sender === "ai" ? agentInfo.agent_name : message.sender}</Text>
+                                        {/* do not format as markdown until text content streaming is finished */}
+                                        {message.done ? <Markdown remarkPlugins={[remarkGfm]}>{message.text}</Markdown> : <Text>{message.text}</Text>}
+                                        {message.sender === "ai" && message.search_metadata && <SearchInfo searchData={message.search_metadata}/>}
+                                    </TextContent>
+                                ))
+                            }
+                        </div>
+                    </div>
+                    <div className="pf-v5-c-panel__footer" style={{"width": "100%"}}>
+                        <TextInput
+                            type="text"
+                            value={chatInput}
+                            id="agent-chat-input"
+                            name="agent-chat-input"
+                            onChange={(e) => setChatInput(e.target.value)}
+                            onKeyDown={handleChatKeyDown}
+                            placeholder="Write a message to the agent. Press ENTER to send..."
+                        />
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
---
+++ src/components/Main/Main.tsx
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import {
+  Button,
+  Form,
+  FormGroup,
+  FileUpload,
+  TextInput,
+  Panel,
+  PanelMain,
+  PanelMainBody,
+  Title,
+  Modal,
+  ModalVariant,
+} from '@patternfly/react-core';
+import { Table, Thead, Tr, Th, Tbody, Td } from '@patternfly/react-table';
+import { Link, useNavigate } from 'react-router-dom';
+
+import AddCircleIcon from "@patternfly/react-icons/dist/esm/icons/add-circle-o-icon"
+
+export const Main = () => {
+  const [data, setData] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [agentData, setAgentData] = useState({
+    name: '',
+    description: '',
+    file: null
+  });
+
+  const navigate = useNavigate();
+
+  const [isModalOpen, setModalOpen] = React.useState(false);
+  const handleModalToggle = (_event) => {
+    setModalOpen(!isModalOpen);
+  };
+
+  const confirmHandler = () => {
+    addAgent();
+    handleModalToggle();
+  }
+
+  useEffect(() => {
+    getAgents();
+  }, []);
+
+  const handleChange = (e) => {
+    const { name, value, files } = e.target;
+    setAgentData({
+      ...agentData,
+      [name]: files ? files[0] : value
+    })
+  }
+
+  const getAgents = () => {
+    axios.get('/api/agents')
+      .then(response => {
+        setData(response.data.data)
+         setLoading(false);
+      })
+      .catch(error => {
+        console.error('Error fetching agents:', error);
+      });
+  };
+
+  const addAgent = () => {
+    const { name, description, system_prompt, file } = agentData;
+    const formData = new FormData();
+    formData.append('name', name);
+    formData.append('description', description);
+    formData.append('system_prompt', system_prompt);
+    formData.append('file', file);
+
+    axios.post('/api/agents', formData)
+      .then(() => {
+        setAgentData({
+          name: '',
+          description: '',
+          system_prompt: '',
+          file: null
+        });
+        getAgents();
+      })
+      .catch(error => {
+        console.error('Error adding agent:', error);
+      })
+  }
+
+  const deleteAgent = (agent) => {
+    axios.delete('/api/agents/' + agent.target.id)
+      .then(() =>
+        getAgents()
+      )
+      .catch(error => {
+        console.error('Error deleting agent:', error);
+      })
+  }
+
+  return (
+    <Panel>
+      <PanelMain>
+        <PanelMainBody>
+          {loading ? (
+              <p>Loading Agents...</p>
+          ) : (
+            <div style={{"width": "90%", "display": "flex", "flexDirection": "column", "marginLeft": "2.5rem"}}>
+              <div style={{"display": "flex", "justifyContent": "end", "paddingTop": "0.5rem"}}>
+                <Button variant="primary" onClick={handleModalToggle} icon={<AddCircleIcon/>}>
+                  Add Agent
+                </Button>
+              </div>
+              <div style={{"marginTop": "2.5rem"}}>
+              <Title headingLevel="h1" style={{"paddingBottom": "1.5rem"}}>Available Agents</Title>
+              <Table aria-label="Simple table">
+              <Thead>
+                <Tr>
+                  <Th>Name</Th>
+                  <Th>Description</Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {data.map(agent => (
+                  <Tr key={agent.id}>
+                    <Td><Link to={`/${agent.id}`}>{agent.agent_name}</Link></Td>
+                    <Td>{agent.description}</Td>
+                    <Td>
+                      <Button id={agent.id} onClick={() => navigate(`/${agent.id}/chat`)} variant="warning">Chat</Button>
+                    </Td>
+                    <Td>
+                      <Button id={agent.id} onClick={() => navigate(`/${agent.id}`)} variant="secondary">Modify</Button>
+                    </Td>
+                    <Td>
+                      <Button id={agent.id} onClick={deleteAgent} variant="danger">Delete</Button>
+                    </Td>
+
+                  </Tr>
+                ))}
+              </Tbody>
+              </Table>
+              </div>
+              <Modal
+                variant={ModalVariant.small}
+                title="Create a new Agent"
+                description="Enter the information below to create a new agent."
+                isOpen={isModalOpen}
+                onClose={handleModalToggle}
+                actions={[
+                  <Button key="addAgent" variant="primary" form="add-agent-button" onClick={confirmHandler}>
+                    Confirm
+                  </Button>,
+                  <Button key="cancel" variant="link" onClick={handleModalToggle}>
+                    Cancel
+                  </Button>
+                ]}
+              >
+                  <Form>
+                    <FormGroup>
+                      <FormGroup label="Agent Name" isRequired>
+                        <TextInput id="name" isRequired type="text" name="name" value={agentData.name} onChange={handleChange}/>
+                      </FormGroup>
+
+                      <FormGroup label="Agent Description" isRequired>
+                        <TextInput id="description" isRequired type="text" name="description" value={agentData.description} onChange={handleChange} />
+                      </FormGroup>
+
+                      <FormGroup label="System Prompt" isRequired>
+                        <TextInput id="prompt" isRequired type="text" name="system_prompt" value={agentData.system_prompt} onChange={handleChange} />
+                      </FormGroup>
+                    </FormGroup>
+                  </Form>
+              </Modal>
+            </div>
+          )}
+        </PanelMainBody>
+      </PanelMain>
+    </Panel>
+  );
+}
